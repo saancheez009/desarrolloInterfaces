a2e6aecd83bcc16ecc4b51d54cc8b2857ca35427
saancheez009 <114072102+saancheez009@users.noreply.github.com>
2023-12-06 23:26:06 +0100

    .

--- Calculadora/Calculadora/MainPage.xaml.cs
+++ Calculadora/Calculadora/MainPage.xaml.cs
@@ -9,5 +9,19 @@
             InitializeComponent();
         }
 
+        private void OnClear(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnSquare(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnNumberSelection(object sender, EventArgs e)
+        {
+
+        }
     }
 }
---
+++ Calculadora/Calculadora/ViewModels/CalculadoraVM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora.ViewModels
+{
+    public class CalculadoraVM : BindableObject
+    {
+        private double _firstNumber;
+        private double _secondNumber;
+        private string _operation;
+        private string _result;
+
+        public double FirstNumber
+        {
+            get => _firstNumber;
+            set
+            {
+                if (_firstNumber != value)
+                {
+                    _firstNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public double SecondNumber
+        {
+            get => _secondNumber;
+            set
+            {
+                if (_secondNumber != value)
+                {
+                    _secondNumber = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public string Operation
+        {
+            get => _operation;
+            set
+            {
+                if (_operation != value)
+                {
+                    _operation = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+    }
+}
