8b819d71ec722ae445305a832eb2ecbcf2ed79b5
saancheez009 <114072102+saancheez009@users.noreply.github.com>
2023-12-14 21:57:46 +0100

    .

---
+++ Unidad11/Unidad11/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Unidad11
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
