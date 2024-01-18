de70cbc6775bd70f5f8c16c05df2782735fdf307
saancheez009 <114072102+saancheez009@users.noreply.github.com>
2024-01-18 14:37:50 +0100

    f

--- Unidad12/Models/clsMensajeUsuario.cs
+++ Unidad12/Models/clsMensajeUsuario.cs
@@ -2,6 +2,39 @@
 {
     public class clsMensajeUsuario
     {
+        #region atributos
+        private string nombreUsuario;
+        private string mensajeUsuario;
+        #endregion
 
+        #region constructores
+        public clsMensajeUsuario()
+        {
+            nombreUsuario = "";
+            mensajeUsuario = "";
+
+        }
+
+        public clsMensajeUsuario(string nombreUsuario, string mensajeUsuario)
+        {
+            this.nombreUsuario = nombreUsuario;
+            this.mensajeUsuario = mensajeUsuario;
+        }
+        #endregion
+
+        #region propiedades
+
+        public String NombreUsuario
+        {
+            get { return nombreUsuario; }
+            set { nombreUsuario = value; }
+        }
+
+        public String MensajeUsuario
+        {
+            get { return mensajeUsuario; }
+            set { mensajeUsuario = value; }
+        }
+        #endregion
     }
 }
