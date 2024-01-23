2f802a7b36ee079e815b41a0fdc9924cf8c0dc0f
saancheez009 <114072102+saancheez009@users.noreply.github.com>
2024-01-23 14:38:55 +0100

    .

--- Unidad12/Unidad12/Pages/Index.cshtml.cs
+++ Unidad12/Unidad12/Pages/Index.cshtml.cs
@@ -1,15 +1,25 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.SignalR.Client;
 
 namespace Unidad12.Pages
 {
     public class IndexModel : PageModel
     {
+
+        private readonly HubConnection _hubConnection;
         private readonly ILogger<IndexModel> _logger;
 
         public IndexModel(ILogger<IndexModel> logger)
         {
             _logger = logger;
+            _hubConnection= new HubConnectionBuilder()
+                .WithUrl("http://192.168.0.165:5151")
+                .Build();
+            _hubConnection.On<string>("ReceiveMessage", (message) => 
+            {
+                chatMessages.Text += $"{Environment.NewLine}{message}";
+            });
         }
 
         public void OnGet()
