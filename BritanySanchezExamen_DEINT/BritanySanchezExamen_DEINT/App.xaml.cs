c227125835f090feb5967de2209be7d25d1c08a1
saancheez009 <114072102+saancheez009@users.noreply.github.com>
2024-02-01 12:25:06 +0100

    .

---
+++ BritanySanchezExamen_DEINT/BritanySanchezExamen_DEINT/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BritanySanchezExamen_DEINT
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ BritanySanchezExamen_DEINT/BritanySanchezExamen_DEINT/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace BritanySanchezExamen_DEINT
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
